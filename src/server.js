6a4268437ec18c9171d3e29fa9cb0fdf2330ae2f
Muneer Zargar <zargarmuneer@yahoo.com>
2020-04-14 12:39:10 +0200

    lesson 1 example

--- src/server.js
+++ src/server.js
@@ -11,5 +11,16 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.get('/', (req, res) => {
+  res.send({ message: 'hello' })
+})
 
-export const start = () => {}
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'ok' })
+})
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
