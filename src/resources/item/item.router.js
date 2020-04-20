efe2bd04b2deedc7d918087dafb3b641c480cbe9
Muneer Zargar <zargarmuneer@yahoo.com>
2020-04-20 11:57:40 +0200

    lesson2

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,20 @@
 import { Router } from 'express'
 
+const mockController = (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'i am working' })
+}
 const router = Router()
+
+router
+  .route('/')
+  .get(mockController)
+  .post(mockController)
+
+router
+  .route('/:id')
+  .put(mockController)
+  .delete(mockController)
+  .get(mockController)
 
 export default router
--- src/server.js
+++ src/server.js
@@ -4,8 +4,14 @@ import morgan from 'morgan'
 import config from './config'
 import cors from 'cors'
 import { connect } from './utils/db'
+import { itemRouter } from './resources/item/item.router'
 
 export const app = express()
+
+// const log = (req, res, next) => {
+//   console.log(req.body)
+//   next()
+// }
 
 app.disable('x-powered-by')
 
@@ -13,7 +19,17 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.use('/api/item', itemRouter)
 
+// app.get('/', (req, res) => {
+//   console.log('get request')
+//   res.send({ message: 'hello express' })
+// })
+
+// app.post('/', (req, res) => {
+//   console.log(req.body)
+//   res.send(req.body)
+// })
 export const start = async () => {
   try {
     await connect()
