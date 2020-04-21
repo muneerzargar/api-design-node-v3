7728808ed2a3d145a0e61939163fa14c2435dd2b
Muneer Zargar <zargarmuneer@yahoo.com>
2020-04-21 08:53:14 +0200

    lesson-3

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,33 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50
+    },
+    status: {
+      type: String,
+      default: 'active',
+      enum: ['active', 'complete', 'pastdue'],
+      required: true
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+      ref: 'user',
+      required: true,
+      type: mongoose.SchemaTypes.ObjectId
+    },
+    list: {
+      type: mongoose.SchemaTypes.ObjectId,
+      required: true,
+      ref: 'list'
+    }
+  },
+  { timestamps: true }
+)
+itemSchema.index({ list: 1, name: 1 }, { unique: true })
 export const Item = mongoose.model('item', itemSchema)
--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -6,7 +6,7 @@ const router = Router()
 // /api/item
 router
   .route('/')
-  .get(controllers.getMany)
+  .get(controllers.getOne)
   .post(controllers.createOne)
 
 // /api/item/:id
